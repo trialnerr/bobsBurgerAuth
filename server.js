2d7b91e1ae1a0e1a6b94993a24341a6e632b63c5
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-17 13:20:24 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+const express  = require('express');
+const app      = express();
+const port     = process.env.PORT || 8080;
+const mongoose = require('mongoose'); 
+const passport = require('passport');  
+const flash    = require('connect-flash'); 
+
+const morgan       = require('morgan'); // log requests to the console
+const cookieParser = require('cookie-parser'); // read cookies
+const session      = require('express-session'); //no longer uses cookie parser
+
+const configDB = require('./config/database.js');
+let db;
+
+// configuration ===============================================================
+mongoose.connect(configDB.url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  require('./app/routes.js')(app, passport, db);
+}); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(express.json()); // get information from html forms
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'skibidi', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
